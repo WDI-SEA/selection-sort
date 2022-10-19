a657d3ae8f12cbe35a15c3bc8a1760b276f60031
JasonXChen <jasonxchen888@gmail.com>
2022-10-19 18:57:13 -0400

    Utilize python multiple assignment

--- sort.py
+++ sort.py
@@ -12,9 +12,7 @@ def selection_sort(li):
 		for i in range(start, len(li)):
 			if li[i] < li[storedIndex]:
 				storedIndex = i
-		startingNum = li[start]
-		li[start] = li[storedIndex]
-		li[storedIndex] = startingNum
+		li[start], li[storedIndex] = li[storedIndex], li[start]
 				
 # for testing
 li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
