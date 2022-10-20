9d4394d95bb1bbe43d875c325c8024c5d85f3b80
joshdewhurst <joshuardewhurst@gmail.com>
2022-10-20 01:17:57 -0700

    josh deliverable

--- sort.py
+++ sort.py
@@ -6,8 +6,13 @@ def is_sorted(ls):
   return True
 
 def selection_sort(li):
-    # implement your algorithm here
-     pass
+	# implement your algorithm here
+	for beg in range(0, len(li)):
+		index = beg
+		for i in range(beg, len(li)):
+			if li[i] < li[index]:
+				index = i
+		li[beg], li[index] = li[index], li[beg]
 
 # for testing
 li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
