38882102768ab9df827d5d0051d42c7c5ddbc6ee
aimeepham14 <aimee.pham14@gmail.com>
2022-10-20 08:57:13 -0700

    pseudo code and selection sort

--- sort.py
+++ sort.py
@@ -6,8 +6,24 @@ def is_sorted(ls):
   return True
 
 def selection_sort(li):
-    # implement your algorithm here
-     pass
+  # once we only have 1 item left on the list, we don't need to do a comparison sort to it
+    indexing_length = range(0, len(li -1 ))
+
+    #everytime we do an iteration, we want the default first value to be the min
+    for i in indexing_length:
+      min_value = i
+      # all of the items to the right 
+      # go through all of the elements, and if something is smaller, swap it
+      for j in range(i+1, len(li)):
+        if li[j] < li[min_value]:
+          min_value = j
+      # if we find a value that is the default and not the smallest already, switch the items
+      if min_value != i:
+        li[min_value], li[i] = li[i], li[min_value]
+    
+    return li
+
+     
 
 # for testing
 li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
