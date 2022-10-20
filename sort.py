446a40ad6eb25de1ee9fdadfef9d0c3a83851055
average-alien <allentang113@gmail.com>
2022-10-19 17:32:47 -0700

    selection sorting

--- sort.py
+++ sort.py
@@ -6,11 +6,26 @@ def is_sorted(ls):
   return True
 
 def selection_sort(li):
-    # implement your algorithm here
-     pass
+  # implement your algorithm here
+  # keep track of how many items have been sorted
+  sorted = 0
+  # while the number of sorted items is less than the length of our list
+  while sorted < len(li):
+    # use # we have sorted as our starting/current index
+    j = sorted
+    # find the smallest item in the unsorted items by comparing our current index to every other index
+    for i in range(j + 1, len(li)):
+      # if our current value is greater than the other value, store the other value's index instead
+      if li[j] > li[i]:
+        j = i
+    # once we have found our smallest unsorted value, swap its place with the end of our sorted values
+    li[sorted], li[j] = li[j], li[sorted]
+    # increment our number of sorted values
+    sorted += 1
 
 # for testing
-li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
+li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49, 30]
 
 selection_sort(li)
+print(li)
 print(is_sorted(li)) # should return True
