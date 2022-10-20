9c07e0194c90fe822060c1a6815dd1fd5eef0e63
brandon-w1205 <bwu1205@gmail.com>
2022-10-20 09:45:23 -0700

    changes swapping logic to not need placeHolder

--- sort.py
+++ sort.py
@@ -8,17 +8,19 @@ def is_sorted(ls):
 def selection_sort(li):
     # implement your algorithm here
     largestNum = 0
-    placeHolder = None
+    # placeHolder = None
     endingIndex = len(li) - 1
     while endingIndex != -1:
       for index in range(endingIndex):
         if li[index + 1] > li[largestNum]:
           largestNum = index + 1
-      placeHolder = li[endingIndex]
-      li[endingIndex] = li[largestNum]
-      li[largestNum] = placeHolder
+      # placeHolder = li[endingIndex]
+      # li[endingIndex] = li[largestNum]
+      # li[largestNum] = placeHolder
+      li[largestNum], li[endingIndex] = li[endingIndex], li[largestNum]
       largestNum = 0
       endingIndex -= 1
+    print(li)
 
 # for testing
 li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
