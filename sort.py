7dcc91faa3763ea74de889a4da0d4aecdca4e2f2
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-10-19 19:53:27 -0500

    all done

--- sort.py
+++ sort.py
@@ -7,7 +7,17 @@ def is_sorted(ls):
 
 def selection_sort(li):
     # implement your algorithm here
-     pass
+    
+
+    for i in range(len(li)):
+      min = li[i]
+      for j in range(len(li)-i):
+        if(min>li[j]):
+          min = li[j]
+
+      li[i] = min
+
+      
 
 # for testing
 li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
