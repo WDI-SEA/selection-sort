ae7ac327a2acf5efcaf3a4f53cbaa384f358ec43
Eijah Kubes <kubeshauseli17@gmail.com>
2022-10-20 00:32:23 -0700

    update

--- sort.py
+++ sort.py
@@ -6,11 +6,21 @@ def is_sorted(ls):
   return True
 
 def selection_sort(li):
-    # implement your algorithm here
-     pass
-
+    # i indicates how many items were sorted
+    for i in range(len(li)-1):
+        # To find the minimum value of the unsorted segment
+        # We first assume that the first element is the lowest
+        min_index = i
+        # We then use j to loop through the remaining elements
+        for j in range(i+1, len(li)-1):
+            # Update the min_index if the element at j is lower than it
+            if li[j] < li[min_index]:
+                min_index = j
+        # After finding the lowest item of the unsorted regions, swap with the first unsorted item
+        li[i], li[min_index] = li[min_index], li[i]
 # for testing
 li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
 
 selection_sort(li)
 print(is_sorted(li)) # should return True
+print(li)
