d1b9266a04633417c26f958774904ca8f4a888f2
elek8871 <elekmichelle@gmail.com>
2022-10-20 10:35:56 -0600

    solution

--- sort.py
+++ sort.py
@@ -8,11 +8,11 @@ def is_sorted(ls):
 def selection_sort(li):
     # implement your algorithm here
     # set current index to minumum
-    # iterate to the right
-    for i in range(li):
+    # iterate to the right- loop ove the list 
+    for i in range(len(li)-1): #needed len(li -1) not len(li)
       min_index = i
       # check if current min is > next item in array
-      for j in range (i +1 , li):
+      for j in range (min_index +1 , len(li)):
         if  li[j] < li[min_index]:
           # if it is set it as new min and switch places
           min_index = j
@@ -20,6 +20,7 @@ def selection_sort(li):
       # cont through the array until all items are sorted
 
  
+
 
 # for testing
 li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
