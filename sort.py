a754e50c8cdf5255d85bea0b547db45a6f28bea8
Moodybleu <meganbenn27@icloud.com>
2022-10-20 09:47:10 -0700

    Compeletes deliverable

--- sort.py
+++ sort.py
@@ -7,10 +7,22 @@ def is_sorted(ls):
 
 def selection_sort(li):
     # implement your algorithm here
-     pass
+    # loop over the list and keep track of first unsorted
+    for i in range(len(li) - 1):
+      # this divides the sorted and unsorted parts of the list
+      first_unsorted = i
+      # smallest index encountered
+      min_index = first_unsorted
+      # nested loop will iterate up and find the smallest unsorted
+      for j in range(first_unsorted + 1, len(li)):
+        # compare current with smallest index, and store current as smallest if needed
+        if li[j] < li[min_index]:
+          min_index = j
+      li[first_unsorted], li[min_index] = li[min_index], li[first_unsorted]
 
 # for testing
 li = [44, 41, 35, 34, 7, 8, 44, 38, 28, 44, 16, 31, 13, 31, 42, 19, 2, 47, 32, 17, 14, 27, 30, 4, 41, 39, 37, 30, 42, 43, 10, 3, 48, 16, 11, 47, 9, 40, 22, 23, 9, 2, 35, 6, 7, 5, 45, 42, 24, 49]
 
 selection_sort(li)
+print(li)
 print(is_sorted(li)) # should return True
